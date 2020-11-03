07a85c4b68f112b05131cfed42d9f6be657bbcbe
Markus Breckner <mbreckner@modyo.com>
2020-11-03 13:51:19 -0500

    feat(swagger): Add 404 to allowed responses

--- core/src/main/java/com/modyo/ms/commons/core/constants/HandledHttpStatus.java
+++ core/src/main/java/com/modyo/ms/commons/core/constants/HandledHttpStatus.java
@@ -6,15 +6,16 @@ import org.springframework.http.HttpStatus;
 
 public class HandledHttpStatus {
   private static final HttpStatus[] list = {
-    HttpStatus.OK,
-    HttpStatus.NO_CONTENT,
-    HttpStatus.BAD_REQUEST,
-    HttpStatus.UNAUTHORIZED,
-    HttpStatus.FORBIDDEN,
-    HttpStatus.METHOD_NOT_ALLOWED,
-    HttpStatus.UNSUPPORTED_MEDIA_TYPE,
-    HttpStatus.UNPROCESSABLE_ENTITY,
-    HttpStatus.INTERNAL_SERVER_ERROR
+      HttpStatus.OK,
+      HttpStatus.NO_CONTENT,
+      HttpStatus.BAD_REQUEST,
+      HttpStatus.UNAUTHORIZED,
+      HttpStatus.FORBIDDEN,
+      HttpStatus.NOT_FOUND,
+      HttpStatus.METHOD_NOT_ALLOWED,
+      HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+      HttpStatus.UNPROCESSABLE_ENTITY,
+      HttpStatus.INTERNAL_SERVER_ERROR
   };
 
   public static List<HttpStatus> getList() {
