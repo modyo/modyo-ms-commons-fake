0d93cbf0b41fa4df5c27301153339cde586f1f52
Markus Breckner <mbreckner@modyo.com>
2020-11-06 19:20:49 -0500

    feat(db): Setup AuditDb

---
+++ audit/src/main/java/com/modyo/ms/commons/audit/config/AuditDbConfig.java
@@ -0,0 +1,73 @@
+package com.modyo.ms.commons.audit.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+    entityManagerFactoryRef = "auditEntityManagerFactory",
+    transactionManagerRef = "auditTransactionManager",
+    basePackages = { "com.modyo.ms.commons.audit.persistence" }
+)
+@RequiredArgsConstructor
+public class AuditDbConfig {
+
+  private static final String PERSISTENCE_ADAPTER_PACKAGE = "com.modyo.ms.commons.audit.persistence";
+  private static final String PROPERTIES_PREFIX = "spring.datasource.audit";
+  private final Environment env;
+
+  @Bean(name = "auditDataSource")
+  @ConfigurationProperties(prefix = "spring.datasource.audit")
+  public DataSource auditDataSource() {
+    HikariDataSource dataSource
+        = new HikariDataSource();
+    Optional.ofNullable(env.getProperty(PROPERTIES_PREFIX.concat(".driverClassName")))
+        .ifPresent(dataSource::setDriverClassName);
+    dataSource.setJdbcUrl(env.getProperty(PROPERTIES_PREFIX.concat(".url")));
+    dataSource.setUsername(env.getProperty(PROPERTIES_PREFIX.concat(".username")));
+    dataSource.setPassword(env.getProperty(PROPERTIES_PREFIX.concat(".password")));
+
+    return dataSource;
+  }
+
+  @Bean(name = "auditEntityManagerFactory")
+  public LocalContainerEntityManagerFactoryBean
+  auditEntityManagerFactory(
+      EntityManagerFactoryBuilder builder,
+      @Qualifier("auditDataSource") DataSource dataSource
+  ) {
+    Map<String,String> properties = new HashMap<>();
+    properties.put("hibernate.hbm2ddl.auto", env.getProperty(PROPERTIES_PREFIX.concat(".ddl-auto")));
+    return builder
+        .dataSource(dataSource)
+        .packages(PERSISTENCE_ADAPTER_PACKAGE)
+        .persistenceUnit("audit")
+        .properties(properties)
+        .build();
+  }
+
+  @Bean(name = "auditTransactionManager")
+  public PlatformTransactionManager auditTransactionManager(
+      @Qualifier("auditEntityManagerFactory") EntityManagerFactory
+          auditEntityManagerFactory
+  ) {
+    return new JpaTransactionManager(auditEntityManagerFactory);
+  }
+}
---
+++ audit/src/test/java/com/modyo/ms/commons/audit/persistence/CreateAuditLogServiceAdapterTest.java
@@ -0,0 +1,55 @@
+package com.modyo.ms.commons.audit.persistence;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import com.modyo.ms.commons.audit.service.ChangeType;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.logging.LogLevel;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ContextConfiguration;
+
+@DataJpaTest
+@ContextConfiguration(classes = {
+    CreateAuditLogServiceAdapter.class,
+    AuditJpaRepository.class
+})
+@EnableJpaRepositories
+@EntityScan
+class CreateAuditLogServiceAdapterTest {
+
+  @Autowired
+  private AuditJpaRepository auditJpaRepository;
+
+  private CreateAuditLogServiceAdapter adapterUnderTest;
+
+  @BeforeEach
+  void setUp() {
+    adapterUnderTest = new CreateAuditLogServiceAdapter(auditJpaRepository);
+  }
+
+  @Test
+  void logInfo_success() {
+    adapterUnderTest.logInfo("id", new Object(), new Object(), ChangeType.CHANGE_STATUS, "changed");
+
+    List<AuditJpaEntity> auditJpaEntityList = auditJpaRepository.findAll();
+    assertThat(auditJpaEntityList.size(), is(1));
+    assertThat(auditJpaEntityList.get(0).getLogLevel(), is(LogLevel.INFO));
+  }
+
+  @Test
+  void logError_success() {
+    adapterUnderTest.logError("id", new Object(), new Object(), ChangeType.CHANGE_STATUS, "changed");
+
+    List<AuditJpaEntity> auditJpaEntityList = auditJpaRepository.findAll();
+    assertThat(auditJpaEntityList.size(), is(1));
+    assertThat(auditJpaEntityList.get(0).getLogLevel(), is(LogLevel.ERROR));
+  }
+
+
+}
