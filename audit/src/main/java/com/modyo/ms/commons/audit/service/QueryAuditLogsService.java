1587bb7cb7c620c972de1d3c2873ac75c9f303b7
Markus Breckner <mbreckner@modyo.com>
2020-12-17 11:06:15 +0100

    feat(audit): QueryAuditLogsService -> return Page instead of List

--- audit/src/main/java/com/modyo/ms/commons/audit/service/QueryAuditLogsService.java
+++ audit/src/main/java/com/modyo/ms/commons/audit/service/QueryAuditLogsService.java
@@ -1,11 +1,11 @@
 package com.modyo.ms.commons.audit.service;
 
 import com.modyo.ms.commons.audit.persistence.AuditJpaEntity;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import java.util.List;
 
 public interface QueryAuditLogsService {
 
-  List<AuditJpaEntity> loadByParent(String parentId, String parentType, Pageable page);
+  Page<AuditJpaEntity> loadByParent(String parentId, String parentType, Pageable page);
 
 }
