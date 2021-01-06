a42dc664f9d37ab5b8ed135044331c40b69d0328
Markus Breckner <mbreckner@modyo.com>
2021-01-06 16:11:48 -0500

    feat(audit): More query params for query audits

--- audit/src/main/java/com/modyo/ms/commons/audit/persistence/QueryAuditLogServiceAdapter.java
+++ audit/src/main/java/com/modyo/ms/commons/audit/persistence/QueryAuditLogServiceAdapter.java
@@ -1,7 +1,12 @@
 package com.modyo.ms.commons.audit.persistence;
 
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.exact;
+
 import com.modyo.ms.commons.audit.service.QueryAuditLogsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,7 +18,26 @@ class QueryAuditLogServiceAdapter implements QueryAuditLogsService {
   private final AuditJpaRepository auditJpaRepository;
 
   @Override
-  public Page<AuditJpaEntity> loadByParent(String parentId, String parentType, Pageable page) {
-    return auditJpaRepository.findAllByAuditableParentIdAndAuditableParentType(parentId, parentType, page);
+  public Page<AuditJpaEntity> loadByParent(AuditQuery auditQuery, Pageable page) {
+    ExampleMatcher matcher = ExampleMatcher.matching()
+        .withMatcher("auditableParentId", exact())
+        .withMatcher("auditableParentType", contains().ignoreCase())
+        .withMatcher("auditable_type", contains())
+        .withMatcher("changeType", contains())
+        .withMatcher("event", contains())
+        .withMatcher("logType", contains())
+        .withIgnoreCase()
+        .withIgnoreNullValues();
+
+    AuditJpaEntity exampleAuditEntity = new AuditJpaEntity();
+    exampleAuditEntity.setAuditableParentId(auditQuery.getParentId());
+    exampleAuditEntity.setAuditableParentType(auditQuery.getParentType());
+    exampleAuditEntity.setAuditableType(auditQuery.getType());
+    exampleAuditEntity.setChangeType(auditQuery.getChangeType());
+    exampleAuditEntity.setEvent(auditQuery.getEvent());
+    exampleAuditEntity.setLogType(auditQuery.getLogType());
+
+    Example<AuditJpaEntity> queryAudit = Example.of(exampleAuditEntity, matcher);
+    return auditJpaRepository.findAll(queryAudit, page);
   }
 }
--- audit/src/main/java/com/modyo/ms/commons/audit/service/QueryAuditLogsService.java
+++ audit/src/main/java/com/modyo/ms/commons/audit/service/QueryAuditLogsService.java
@@ -1,11 +1,29 @@
 package com.modyo.ms.commons.audit.service;
 
+import com.modyo.ms.commons.audit.AuditLogType;
 import com.modyo.ms.commons.audit.persistence.AuditJpaEntity;
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface QueryAuditLogsService {
 
-  Page<AuditJpaEntity> loadByParent(String parentId, String parentType, Pageable page);
+  Page<AuditJpaEntity> loadByParent(AuditQuery auditQuery, Pageable page);
+
+  @Getter
+  @Builder(builderMethodName = "hiddenBuilder")
+  class AuditQuery {
+    private String parentId;
+    private String parentType;
+    private String type;
+    private String changeType;
+    private String event;
+    private AuditLogType logType;
+
+    public static AuditQueryBuilder builder(String parentId, String parentType) {
+      return hiddenBuilder().parentId(parentId).parentType(parentType);
+    }
+  }
 
 }
