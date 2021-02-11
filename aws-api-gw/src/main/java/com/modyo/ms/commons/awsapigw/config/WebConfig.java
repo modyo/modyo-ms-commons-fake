39aa01b6beb665ebf787f2c6dc1e9c5d6f403b98
Markus Breckner <mbreckner@modyo.com>
2021-02-11 15:24:52 -0500

    fix(serializing)

--- aws-api-gw/src/main/java/com/modyo/ms/commons/awsapigw/config/WebConfig.java
+++ aws-api-gw/src/main/java/com/modyo/ms/commons/awsapigw/config/WebConfig.java
@@ -12,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
